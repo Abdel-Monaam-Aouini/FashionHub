95986232457fd02ea3ef1f7a7a8c3b93027bfa96
Abdel-Monaam-Aouini <clemonaam@gmail.com>
2023-07-08 11:37:48 +0100

    clean

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import postcssNesting from 'postcss-nesting';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
