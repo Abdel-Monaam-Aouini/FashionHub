7f7c5a4714c13ae0e307e320f65f705c00cfc97d
Abdel-Monaam-Aouini <clemonaam@gmail.com>
2023-07-08 11:37:31 +0100

    fix sass

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import postcssNesting from 'postcss-nesting';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react({
       // Add this line
-      include: "**/*.tsx",
+      include: '**/*.tsx',
     }),
   ],
   server: {
